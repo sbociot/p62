88d2f5b7e1da3a7e12248592234d6df45df24c70
SteveBocicot <Steve@stevemac.isimtl.local>
2017-01-23 12:38:26 -0500

    structure

--- base_php/base_header.php
+++ base_php/base_header.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang ="fr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>YS Electonics</title>
   </head>
   <body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="base_script/master.js"></script>
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+//Tête de page
 require_once'base_php/base_header.php';
+//Menu principal
 require_once'base_php/menu.php';
+/****Contenu principal****/
+//fin du body
+require_once'base_php/page_end.php';
+
  ?>
