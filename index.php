ff191d0d83e41ba442e21851d3b8ad516be617af
SteveBocicot <Steve@stevemac.isimtl.local>
2017-01-23 12:17:40 -0500

    modif pas acceuil

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-
+require_once'base_php/base_header.php';
+require_once'base_php/menu.php';
  ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>YS Electronic Store</title>
-    <link rel="stylesheet" href="css/master.css">
-  </head>
-  <body>
-    <header>
-    <?php
-require_once 'base_php/menu.php';
-     ?>
-    </header>
-  </body>
-</html>
